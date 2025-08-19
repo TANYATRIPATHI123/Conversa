1ba8bc9dc23199fa26aa318e423c4c524e4bb168
Tanya <7099.tanya@gmail.com>
2025-08-19 23:28:07 +0530

    last

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,6 @@ const __dirname = path.resolve();
 
 dotenv.config();
 
-import cors from "cors";
-app.use(cors({
-  origin: ["https://conversa-sand.vercel.app/"], // your Vercel domain
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
 
 app.use(express.json());
 app.use(cookieParser())
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || 'https://conversa-nspg.onrender.com',
+  base: process.env.VITE_BASE_PATH || "/Conversa",
   server:{
     proxy:{
       '/api':{
-        target:'https://conversa-nspg.onrender.com',
+        target:'https://localhost:3000',
         secure:false
       }
     }
