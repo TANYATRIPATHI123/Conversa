25fe719ecf02521bdad032263c261f918fa16a7e
Tanya <7099.tanya@gmail.com>
2025-09-01 11:20:29 +0530

    backend changes

--- backend/Socket/socket.js
+++ backend/Socket/socket.js
@@ -7,8 +7,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:['https://conversa-sand.vercel.app/'],
-        methods:["GET","POST"]
+        origin:["*"],
+        methods:["*"],
+        credentials: true
     }
 });
 
